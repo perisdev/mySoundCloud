f0a8988fb48a7bbb9469259fd9f693e6f72edd0c
Santi Peris <perisdev>
2019-11-06 19:55:39 +0100

    add player section: design, view and logic

--- js/app.js
+++ js/app.js
@@ -1,47 +1,50 @@
-/* START - DECLARATIONs
-  ----------------------------------------------------------*/
-
-/* VARIABLEs */
+/* GLOBAL VARIABLEs */
 let listTracks = [];
-let playerTrack = null;
-let currentStream = null;
-
-/* CLASSEs */
-class Track {
-  constructor(artist, title, track, duration, cover, release_year) {
-    this.artist = artist;
-    this.title = title;
-    this.track = track;
-    this.duration = duration;
-    this.cover = cover;
-    this.release_year = release_year;
-    this.views = {
-      0: null,  // result list view
-      1: {
-        view: null,  // player view
-        rotation: 0
-      }
-    };
-  }
-}
+let player = new Player;
 
 /* FUNCTIONs */
 const createNodeList = (track) => {
-  let node = (`
-  <div class="track">
-    <img src="${(track.cover) ? track.cover : './img/noCover.png'}" alt="" class="track_img">
+  let node = `
+  <div data-id="${track.id}" class="track" draggable="true" ondragstart="onDragStart(event);">
+    <img src="${track.cover}" alt="" class="track_img" draggable="false">
     <p>${track.title}</p>
     <p>${track.artist}</p>
   </div>
-  `);
+  `;
 
   document.getElementById('list').innerHTML += node;
+}
+
+const createNodeDetail = (track) => {
+  let node = `
+  <img src="${track.cover}" alt="" class="dCover">
+  <div class="artist">
+  <p class="dt">Artist</p>
+    <p class="dd">${track.artist}</p>
+  </div>
+  <div class="title">
+    <p class="dt">Title</p>
+    <p class="dd">${track.title}</p>
+    </div>
+  <div class="year">
+  <p class="dt">Year</p>
+  <p class="dd">${track.release_year}</p>
+  </div> 
+  `;
+
+  document.getElementById('detail').innerHTML = node;
+}
+
+const addListTracksViews = () => {
+  let tracks = document.getElementsByClassName('track');
+  for (let i = 0; i < tracks.length; i++) {
+    listTracks[i].listView = tracks[i];
+  }
 }
 
 const resetLastSearch = () => {
   list.innerHTML = '';
   listTracks = [];
-  playerTrack = null;
 };
 
 const search = (input) => {
@@ -52,65 +55,48 @@ const search = (input) => {
     limit: 30
   }).then(tracks => {
     for (let i = 0; i < tracks.length; i++) {
-      listTracks.push(new Track(tracks[i].permalink, tracks[i].title, tracks[i].id, tracks[i].duration, tracks[i].artwork_url));
+      listTracks.push(new Track(i, tracks[i].permalink, tracks[i].title, tracks[i].id, tracks[i].duration, tracks[i].artwork_url));
     }
 
     listTracks.map((item) => createNodeList(item));
-
-    console.log(listTracks);
-    console.log(tracks);
+    addListTracksViews();
   });
 }
 
-/** my rotate control --------------------------------------*/
-const rotateCover = (angle) => {
-  let lastCoverPos = angle;
-  let lastAxisPos = 0;
-
-  return setInterval(() => {
-    if (lastCoverPos == 360){
-      lastCoverPos = 0;
-      lastAxisPos = 0;
-    } else {
-      lastCoverPos++;
-      lastAxisPos--;
-    }
- 
-    document.getElementById("cover").style.transform = `rotate(${lastCoverPos}deg)`;
-    document.getElementById("axis").style.transform = `rotate(${lastAxisPos}deg)`;
-  }, 6);
+// milisecons to min:secs |->  61000 -> 01:01
+const getMinSec = (timeMs) => {
+  let minSec = moment.duration(timeMs);
+  let min = (minSec.minutes() < 10)? '0' + minSec.minutes():minSec.minutes(); 
+  let sec = (minSec.seconds() < 10)? '0' + minSec.seconds():minSec.seconds();
+  return `${min}:${sec}`;
 }
 
-/** myStream control --------------------------------------*/
-const newStream = track => {
-  let newStream = SC.stream(`/tracks/${track}`);
-  newStream.then(player => player.on('state-change', (e) => {
-    console.log(e);
-  }));
-
-  return newStream;
+/** DRAG & DROP --------------------------------------*/
+// do in the drag
+const onDragStart = (e) => {
+  e.dataTransfer.setData('text', e.target.dataset.id);
 };
 
-//currentStream = newStream('9436047');
+const onDragOver = (e) => {
+  e.preventDefault();
+};
 
-//myStream.then(player => player.play());
-//myStream.then(player => player.pause());
-//myStream.then(player => player.kill());
-//myStream.then(player => player.currentTime());
-//myStream.then(player => player.getDuration());
-//myStream.then(player => player.getState());
-//myStream.then(player => player.getVolume());
-//myStream.then(player => player.setVolume());
+// do in the drop
+const onDrop = (e) => {
+  e.preventDefault();
 
-// SC.stream('/tracks/9436047')
-//   .then(function (player) {
-  //     player.play();
-  //   });
+  const id = listTracks.findIndex((item) => item.id == e.dataTransfer.getData('text'));
+  e.dataTransfer.clearData();
 
-/** ------------------------------------------------------ */
-  
-/*----------------------------------------------------------
-  END - DECLARATIONs */
+  if (id >= 0) {
+    createNodeDetail(listTracks[id]);
+    
+    player.newPlayer(listTracks[id]);
+    player.play();
+    
+    listTracks[id].listView.remove();
+  }
+};
 
 
 /* START - APP  
@@ -120,23 +106,46 @@ SC.initialize({
 });
 
 
-// let myPlay = rotateCover(0);
-// clearInterval(myPlay);
-
 /* LISTENERs
   --------------------------------------------------------- */
 let input = document.getElementById('search');
-let list = document.getElementById('list');
+let btSearch = document.getElementById('btSearch');
 
 input.addEventListener('keyup', (e) => {
   if (e.key === 'Enter' && input.value)
     search(input.value);
 });
 
-list.addEventListener('click', () => {
+btSearch.addEventListener('click', () => {
   if (input.value)
     search(input.value);
 });
 
+document.getElementById('btPlay').addEventListener('click', () => {
+  if (player.state == 'paused')
+    player.play();
+  else if (player.state == 'playing' || player.state == 'loading')
+    player.pause();
+});
 
+document.getElementById('btStart').addEventListener('click', () => {
+  if (player.stream) {
+    player.setPosition(0);
+    player.setBarPosition(0);
+  }
+});
+
+document.getElementById('barTime').addEventListener('click', (e) => {
+
+  if (player.stream) {
+    let bar = e.target;
+
+    let diffPx = e.clientX - bar.offsetLeft;
+    let barPercent = (diffPx * 100) / bar.offsetWidth;
+    let trackMs = (player.duration * barPercent) / 100;
+
+    player.setBarPosition(trackMs);
+    player.setPosition(trackMs);
+  }
+});
 
---
+++ js/player.js
@@ -0,0 +1,153 @@
+/*  PLAYER class
+--------------------------*/
+class Player {
+  constructor() {
+    this.state = '';          // loading | playing | paused | dead | ended
+    this.cover = '';          // cover to disk
+    this.view = null;         // DOM element
+    
+    this.currentTime = 0;     // track current pos
+    this.volume = 0;        
+    this.stream = null;       // current stream
+
+    this.lastCoverPos = 0;    // last cover angule
+    this.lastAxisPos = 0;     // last axis angule
+    this.rotateId = 0;        // id of rotate intervale
+
+    this.barId = 0;           // id of bar interval refresh
+    this.barPosPercent = 0;   // current bar percent
+    this.duration = 0;        // track duration in ms.
+  }
+
+  newPlayer = track => {
+    this.cover = track.cover;
+    this.view = document.getElementById('cover').setAttribute('style', `background-image:url('${this.cover}')`);
+
+    this.duration = track.duration;
+    this.currentTime = 0;
+
+    this.stream = SC.stream(`/tracks/${track.track}`);
+
+    // we subscribe to player state change
+    this.stream.then(player => player.on('state-change', (e) => {
+      this.state = e;
+
+      switch (e) {
+        case 'loading':
+        case 'playing':
+          this.rotateStart();
+          this.barPositionStart();
+          break;
+        case 'paused':
+          this.rotateStop();
+          this.barPositionStop();
+          break;
+        case 'dead':
+          this.rotateStop();
+          this.barPositionStop();
+          this.stream = null;
+          break;
+        case 'ended':
+          this.rotateStop();
+          this.barPositionStop();
+          this.stream = null;
+          break;
+        default:
+          break;
+      }
+    }));
+  };
+
+  play = () => this.stream.then(player => player.play());
+  pause = () => this.stream.then(player => player.pause());
+  kill = () => this.stream.then(player => player.kill());
+
+  getCurrentTime = () => this.stream.then(player => {
+    this.currentTime = player.currentTime();
+    return this.currentTime;
+  });
+
+  getDuration = () => this.stream.then(player => {
+    this.duration = player.getDuration();
+    return this.duration;
+  });
+
+  getVolume = () => this.stream.then(player => {
+    this.volume = player.getVolume();
+    return this.volume;
+  });
+
+  setVolume = (vol) => this.stream.then(player => {
+    player.setVolume(vol);
+    this.volume = vol;
+    return this.volume;
+  });
+
+  setPosition = (pos) => this.stream.then(player => {
+    player.seek(pos);
+  });
+
+  // rotate DISK control
+  rotateCover = (angle) => {
+    this.lastCoverPos = angle;
+    this.lastAxisPos = 0;
+
+    return setInterval(() => {
+      if (this.lastCoverPos == 360) {
+        this.lastCoverPos = 0;
+        this.lastAxisPos = 0;
+      } else {
+        this.lastCoverPos++;
+        this.lastAxisPos--;
+      }
+
+      document.getElementById("cover").style.transform = `rotate(${this.lastCoverPos}deg)`;
+      document.getElementById("axis").style.transform = `rotate(${this.lastAxisPos}deg)`;
+    }, 6);
+  }
+
+  rotateStart = () => {
+    (this.rotateId) ? this.rotateStop() : null
+    this.rotateId = this.rotateCover(this.lastCoverPos);
+  }
+
+  rotateStop = () => clearInterval(this.rotateId);
+
+  // BAR TIME control
+  setBarPosition = (posMs) => {
+    if (player.stream) {
+      this.currentTime = posMs;
+      this.barPosPercent = (posMs * 100) / player.duration;
+      this.barPosPercent = (this.barPosPercent >= 99) ? 100 : this.barPosPercent;
+
+      document.getElementById('currentTime').innerHTML = getMinSec(posMs);
+      document.getElementById('durationTime').innerHTML = getMinSec(this.duration);
+      document.getElementById('barTime').setAttribute('style', `background-size: ${this.barPosPercent}% 29px`);
+    }
+  };
+
+  autoRefreshBar = () => {
+    return setInterval(() => {
+      this.getCurrentTime();
+      this.setBarPosition(this.currentTime);
+    }, 1000);
+  }
+
+  barPositionStart = () => this.barId = this.autoRefreshBar();
+  barPositionStop = () => clearInterval(this.barId);
+}
+
+/*  TRACK class
+--------------------------*/
+class Track {
+  constructor(id, artist, title, track, duration, cover, release_year) {
+    this.id = id;
+    this.artist = artist;
+    this.title = title;
+    this.track = track;
+    this.duration = duration;
+    this.cover = (cover) ? cover : './img/noCover.png';
+    this.release_year = (release_year) ? release_year : '-';
+    this.listView = null;
+  }
+}
